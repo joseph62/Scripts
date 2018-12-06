e70a0144a37f3be8149f13a6b40bbf9de7945b88
Sean Joseph <joseph62@egr.msu.edu>
2018-12-06 09:54:47 -0500

    bytes-to-binary renamed to bytes_to_binary and added more robust formatting.

--- pyscripts/bytes_to_binary.py
+++ pyscripts/bytes_to_binary.py
@@ -4,14 +4,22 @@ Read stdin, turn input into 8 bit binary, output binary to stdout
 """
 
 import sys
+import itertools
+
+def chunks(iterable, size):
+    return iter(itertools.zip_longest(*([iter(iterable)] * size)))
 
 def main(argv):
     # Reading stdin and encoding in bytes
     bytes_ = sys.stdin.read().encode()
     # format each byte as a length 8 binary number
-    binary = [ f"{byte:08b}" for byte in bytes_ ]
-    # output the binary
-    sys.stdout.write("".join(binary)) 
+    binary = chunks((f'{byte:08b}' for byte in bytes_), 8)
+    sys.stdout.write(
+        '\n'.join(
+            ' '.join(col for col in row if col) for row in binary
+        )
+    )
+    sys.stdout.write('\n')
     return 0
 
 if __name__ == '__main__':
