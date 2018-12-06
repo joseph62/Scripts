02c1fc63e337d05018d758c810ea104d22a86073
Sean Joseph <joseph62@egr.msu.edu>
2018-12-06 09:01:05 -0500

    replace-name changed to replace_identifier. Also restructured execution.

---
+++ pyscripts/replace_identifier.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python3
+
+import argparse
+import os
+import sys
+
+def get_arguments(args):
+    parser = argparse.ArgumentParser(
+        description="Expand appname identifier into the application name"
+    )
+    parser.add_argument("-p", "--path", default=os.getcwd(),
+        help="The path to work with. Current working directory by default"
+    )
+    parser.add_argument("-i", "--identifier", required=True,
+        help="The identifier to substitute."
+    )
+    parser.add_argument("-s", "--substitute", required=True,
+        help="The value to replace the identifier with."
+    )
+    return parser.parse_args(args)
+
+def replace_identifier(path, identifier, substitute):
+    files = os.listdir(path)
+    replace_pairs = []
+
+    for file in files:
+        new_file = file.replace(identifier, substitute)
+        old_path = os.path.join(path, file)
+        new_path = os.path.join(path, new_file)
+        os.replace(old_path, new_path)
+        replace_pairs.append((file, new_file))
+
+    return replace_pairs
+
+def main(args):
+    args = get_arguments(args)
+    output = replace_identifier(args.path, args.identifier, args.substitute)
+
+    for old_file, new_file in output:
+        print(f'"{old_file}" -> "{new_file}"')
+
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
